879362d035645f03cfad7ec15c887e9dbc651c03
Toufiq <burningraven06@gmail.com>
2018-11-03 13:55:24 +0600

    create observable from keyup event and emulate data binding

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,40 @@
 import { fromEvent } from 'rxjs';
 import {Observable} from 'rxjs';
 
-var theBtn = document.getElementById("theBtn");
 var message = document.getElementById("message");
 
+var theBtn = document.getElementById("theBtn");
 var btnStream = fromEvent(theBtn, "click");
 
 btnStream.subscribe( 
   (e) => {
-    message.innerHTML = "Clicked"
-    console.log("Clicked")
+    message.innerHTML = "Clicked";
+    console.log("Clicked") ;
   },
   (err) => {
-    message.innerHTML = err
-    console.log("Error")
+    message.innerHTML = err ;
+    console.log("Error") ;
   } ,
   () => {
-    message.innerHTML = "Completed"
-    console.log("Completed")
+    message.innerHTML = "Completed" ;
+    console.log("Completed");
   }
 )
 
+var theInput = document.getElementById("theInput");
+var inputStream = fromEvent(theInput, "keyup");
+
+inputStream.subscribe( 
+  (e : any) => {
+    message.innerHTML = e.target.value;
+    console.log("Input stream ", e.target.value) ;
+  },
+  (err) => {
+    message.innerHTML = err ;
+    console.log("Error") ;
+  } ,
+  () => {
+    message.innerHTML = "Completed" ;
+    console.log("Completed");
+  }
+)
